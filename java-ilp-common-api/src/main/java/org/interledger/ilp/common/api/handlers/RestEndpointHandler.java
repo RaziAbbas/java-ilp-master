c5255f6726a99e7acf23ca45c3def7939d780e17
mrmx <mistermx@gmail.com>
2016-10-06 11:55:53 +0200

    Wip auth check

--- java-ilp-common-api/src/main/java/org/interledger/ilp/common/api/handlers/RestEndpointHandler.java
+++ java-ilp-common-api/src/main/java/org/interledger/ilp/common/api/handlers/RestEndpointHandler.java
@@ -1,14 +1,16 @@
 package org.interledger.ilp.common.api.handlers;
 
 import io.netty.handler.codec.http.HttpResponseStatus;
-
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.User;
 import io.vertx.ext.web.RoutingContext;
 import java.util.function.Supplier;
 import org.apache.commons.lang3.StringUtils;
 import org.interledger.ilp.common.api.util.JsonObjectBuilder;
 import org.interledger.ilp.common.api.util.VertxUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Rest endpoint handler
@@ -16,9 +18,13 @@ import org.interledger.ilp.common.api.util.VertxUtils;
  * @author mrmx
  */
 public abstract class RestEndpointHandler extends EndpointHandler {
-
+    private static final Logger log = LoggerFactory.getLogger(RestEndpointHandler.class);
+    
     private final static String PARAM_ENCODE_PLAIN_JSON = "plainjson";
     private final static String MIME_JSON_WITH_ENCODING = "application/json; charset=utf-8";
+
+    private final static String UNAUTHORIZED_ERROR_ID = "UnauthorizedError";
+    private final static String UNAUTHORIZED_ERROR_MSG = "Unknown or invalid account / password";
 
     public RestEndpointHandler(String name) {
         this(name, name);
@@ -67,12 +73,46 @@ public abstract class RestEndpointHandler extends EndpointHandler {
         response(context, HttpResponseStatus.NOT_IMPLEMENTED);
     }
 
+    /**
+     * Check if calling user is authorized and calls handleAuthorized, else
+     * returns forbidden.
+     *
+     * @param context {@code RoutingContext}
+     * @param authority the authority - what this really means is determined by
+     * the specific implementation. It might represent a permission to access a
+     * resource e.g. `printers:printer34` or it might represent authority to a
+     * role in a roles based model, e.g. `role:admin`.
+     */
+    protected void checkAuth(RoutingContext context, String authority) {
+        User user = context.user();
+        if(user == null) {
+            log.warn("No user present in request in checkAuth with {}",authority);
+            forbidden(context);
+        } else {
+            user.isAuthorised(authority, res -> {
+                if (res.succeeded()) {
+                    handleAuthorized(context);
+                } else {
+                    forbidden(context);
+                }
+            });
+        }
+    }
+
+    protected void handleAuthorized(RoutingContext context) {
+        response(context, HttpResponseStatus.NOT_IMPLEMENTED);
+    }
+
     protected Supplier<JsonObject> buildJSON(CharSequence id, CharSequence message) {
         return buildJSONWith("id", id, "message", message);
     }
 
     protected Supplier<JsonObject> buildJSONWith(Object... pairs) {
         return JsonObjectBuilder.create().with(pairs);
+    }
+
+    protected void forbidden(RoutingContext context) {
+        response(context, HttpResponseStatus.FORBIDDEN, buildJSON(UNAUTHORIZED_ERROR_ID, UNAUTHORIZED_ERROR_MSG));
     }
 
     protected void response(RoutingContext context, HttpResponseStatus responseStatus) {
