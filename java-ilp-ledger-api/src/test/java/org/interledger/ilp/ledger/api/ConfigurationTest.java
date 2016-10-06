362e746c9b44bcf7e9a403dfb97e2034b9f24aea
earizon <enrique.arizonbenito@gmail.com>
2016-10-06 17:06:06 +0200

    Clean warnings

--- java-ilp-ledger-api/src/test/java/org/interledger/ilp/ledger/api/ConfigurationTest.java
+++ java-ilp-ledger-api/src/test/java/org/interledger/ilp/ledger/api/ConfigurationTest.java
@@ -3,11 +3,8 @@ package org.interledger.ilp.ledger.api;
 
 import static org.junit.Assert.*;
 
-import org.interledger.ilp.common.api.AbstractMainEntrypointVerticle;
 import org.interledger.ilp.common.config.Config;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 /**
@@ -21,7 +18,7 @@ public class ConfigurationTest {
     @Test
     public void testRunServer() {
         // TODO: Check othe config "stuff" in AbstractMainEntrypointVerticle.initConfig
-        Config.create(); // Will fail if parsing application.conf and/or app.conf fails.
+        Config.create(); // Will raise and exception if parsing application.conf and/or app.conf fails.
         assertTrue(true);
     }
 
