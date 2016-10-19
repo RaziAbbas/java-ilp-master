ac04f9b4cf579060d67d535ed2bcfc78a5d698fb
earizon <enrique.arizonbenito@gmail.com>
2016-10-19 10:16:17 +0200

    Work on Receipt REST API

--- java-ilp-ledger-api/src/main/java/org/interledger/ilp/ledger/api/handlers/ReceiptHandler.java
+++ java-ilp-ledger-api/src/main/java/org/interledger/ilp/ledger/api/handlers/ReceiptHandler.java
@@ -2,31 +2,20 @@ package org.interledger.ilp.ledger.api.handlers;
 
 import io.netty.handler.codec.http.HttpResponseStatus;
 import static io.vertx.core.http.HttpMethod.GET;
-import static io.vertx.core.http.HttpMethod.POST;
-import static io.vertx.core.http.HttpMethod.PUT;
-import io.vertx.core.json.JsonArray;
+
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
-import javax.money.CurrencyUnit;
-import javax.money.Monetary;
-import javax.money.MonetaryAmount;
+
 import org.interledger.ilp.common.api.ProtectedResource;
 import org.interledger.ilp.common.api.auth.impl.SimpleAuthProvider;
 import org.interledger.ilp.common.api.handlers.RestEndpointHandler;
-import org.interledger.ilp.core.AccountUri;
-import org.interledger.ilp.core.ConditionURI;
-import org.interledger.ilp.core.Credit;
-import org.interledger.ilp.core.DTTM;
-import org.interledger.ilp.core.Debit;
-import org.interledger.ilp.core.LedgerInfo;
+
 import org.interledger.ilp.core.LedgerTransfer;
 import org.interledger.ilp.core.TransferID;
 import org.interledger.ilp.core.TransferStatus;
-import org.interledger.ilp.ledger.LedgerFactory;
-import org.interledger.ilp.ledger.impl.simple.SimpleLedgerTransfer;
 import org.interledger.ilp.ledger.impl.simple.SimpleLedgerTransferManager;
 import org.interledger.ilp.ledger.transfer.LedgerTransferManager;
-import org.javamoney.moneta.Money;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,6 +28,8 @@ public class ReceiptHandler extends RestEndpointHandler implements ProtectedReso
 
     private static final Logger log = LoggerFactory.getLogger(ReceiptHandler.class);
     private final static String transferUUID  = "transferUUID";
+    private static final String RECEIPT_TYPE_ED25519 = "ed25519-sha512",
+                                RECEIPT_TYPE_SHA256  = "sha256";
 
     // GET     /transfers/25644640-d140-450e-b94b-badbe23d3389/state|state?type=sha256 
     // PUT /transfers/4e36fe38-8171-4aab-b60e-08d4b56fbbf1/rejection
@@ -54,6 +45,17 @@ public class ReceiptHandler extends RestEndpointHandler implements ProtectedReso
 
     public static ReceiptHandler create() {
         return new ReceiptHandler(); // TODO: return singleton?
+    }
+    
+    private static JsonObject makeTransferStateMessage(String transferId, TransferStatus state, String receiptType) {
+        JsonObject jo = new JsonObject();
+        jo.put("id", transferId);
+        jo.put("state", state);
+        if (receiptType.equals(RECEIPT_TYPE_SHA256)) {
+            String token = ""; // FIXME: sign(sha512(transferId + ':' + state))
+            jo.put("token", token);
+        }
+        return jo;
     }
 
     @Override
@@ -74,66 +76,68 @@ public class ReceiptHandler extends RestEndpointHandler implements ProtectedReso
             forbidden(context);
             return;
         }
-        
-        /* FIXME:
-         *      this.body = yield model.getTransferStateReceipt(
-         *        id.toLowerCase(), receiptType, conditionState)
-         */
-        // REF: getStateResource @ transfers.js
-        String conditionState = context.request().getParam("condition_state");
-        String receiptType = context.request().getParam("type");
-        // REF: getTransferStateReceipt(id, receiptType, conditionState) @ five-bells-ledger/src/models/transfers.js
-        if (receiptType == null) { receiptType = "ed25519-sha512"; }
-        if ( ! receiptType.equals("ed25519-sha512") &&
-             ! receiptType.equals("sha256"        ) &&
-             false ) {
-            throw new RuntimeException("type not in := ed25519-sha512* | sha256 ");
-        }
-        if (receiptType.equals("ed25519-sha512")) {
-            // FIXME: Implement
-            // return makeEd25519Receipt(uri.make('transfer', id), transferState)
-           /*
-            * function makeEd25519Receipt (transferId, transferState) {
-            *   const message = makeTransferStateMessage(transferId, transferState, RECEIPT_TYPE_ED25519)
-            *   return {
-            *     type: RECEIPT_TYPE_ED25519,
-            *     message: message,
-            *     signer: config.getIn(['server', 'base_uri']),
-            *     public_key: config.getIn(['keys', 'ed25519', 'public']),
-            *     signature: sign(hashJSON(message))
-            *   }
-            * }
-            */
-        } else {
-            // FIXME: Implement
-            // return makeSha256Receipt(uri.make('transfer', id), transferState, conditionState)
-            /*
-             * function makeSha256Receipt (transferId, transferState, conditionState) {
-             *   const message = makeTransferStateMessage(transferId, transferState, RECEIPT_TYPE_SHA256)
-             *   const receipt = {
-             *     type: RECEIPT_TYPE_SHA256,
-             *     message: message,
-             *     signer: config.getIn(['server', 'base_uri']),
-             *     digest: sha256(stringifyJSON(message))
-             *   }
-             *   if (conditionState) {
-             *     const conditionMessage = makeTransferStateMessage(transferId, conditionState, RECEIPT_TYPE_SHA256)
-             *     receipt.condition_state = conditionState
-             *     receipt.condition_digest = sha256(stringifyJSON(conditionMessage))
-             *   }
-             *   return receipt
-             * }
-             */
-        }
-        TransferID transferID = new TransferID(context.request().getParam(transferUUID));
+        String transferId = context.request().getParam(transferUUID);
+        TransferID transferID = new TransferID(transferId);
         LedgerTransferManager tm = SimpleLedgerTransferManager.getSingleton();
         TransferStatus status = TransferStatus.NONEXISTENT;
         if (tm.transferExists(transferID)) { 
             LedgerTransfer transfer = tm.getTransferById(transferID);
             status = transfer.getTransferStatus();
         }
-        response(context, HttpResponseStatus.ACCEPTED,
-                buildJSON("result", ((SimpleLedgerTransfer) transfer).toWalletJSONFormat()));
+        
+        /* FIXME:
+         *      this.body = yield model.getTransferStateReceipt(
+         *        id.toLowerCase(), receiptType, conditionState)
+         */
+        // REF: getStateResource @ transfers.js
+        
+        
+
+        String receiptType = context.request().getParam("type");
+        // REF: getTransferStateReceipt(id, receiptType, conditionState) @ five-bells-ledger/src/models/transfers.js
+        if (receiptType == null) { receiptType = RECEIPT_TYPE_ED25519; }
+        if ( ! receiptType.equals(RECEIPT_TYPE_ED25519) &&
+             ! receiptType.equals(RECEIPT_TYPE_SHA256 ) &&
+             true ) {
+            throw new RuntimeException("type not in := "+RECEIPT_TYPE_ED25519+"* | "+RECEIPT_TYPE_SHA256+" ");
+        }
+        JsonObject jo = new JsonObject();
+        String signer = "";      // FIXME: config.getIn(['server', 'base_uri']),
+        if (receiptType.equals(RECEIPT_TYPE_ED25519)) {
+            // REF: makeEd25519Receipt(transferId, transferState) @
+            //      @ five-bells-ledger/src/models/transfers.js
+            JsonObject message = makeTransferStateMessage(transferId, status, RECEIPT_TYPE_ED25519);
+            String public_key = "";  // FIXME: config.getIn(['keys', 'ed25519', 'public']),
+            String signature = "";   // FIXME: sign(hashJSON(message))
+            jo.put("type", RECEIPT_TYPE_ED25519);
+            jo.put("message", message);
+            jo.put("signer", signer);
+            jo.put("public_key", public_key);
+            jo.put(signature, signature);
+        } else {
+            // REF: makeSha256Receipt(transferId, transferState, conditionState) @
+            //      @ five-bells-ledger/src/models/transfers.js
+            JsonObject message = makeTransferStateMessage(transferId, status, RECEIPT_TYPE_SHA256);
+            String digest = "";     // FIXME: sha256(stringifyJSON(message))
+            jo.put("type", RECEIPT_TYPE_SHA256);
+            jo.put("message", message);
+            jo.put("signer", signer);
+            jo.put("digest", digest);
+            String conditionState = context.request().getParam("condition_state");
+            if (conditionState != null) {
+                JsonObject conditionMessage = makeTransferStateMessage(transferId, status, RECEIPT_TYPE_SHA256);
+                String condition_digest = "TODO sha256 of "+conditionMessage; // FIXME sha256(conditionMessage.encode())
+                jo.put("condition_state", conditionState);
+                jo.put("condition_digest", condition_digest);
+            }
+        }
+
+        String response = jo.encode();
+        context.response()
+        .putHeader(HttpHeaders.CONTENT_TYPE, "text/plain")
+        .putHeader(HttpHeaders.CONTENT_LENGTH, ""+response.length())
+        .setStatusCode(HttpResponseStatus.ACCEPTED.code())
+        .end(response);
     }
 
 }
