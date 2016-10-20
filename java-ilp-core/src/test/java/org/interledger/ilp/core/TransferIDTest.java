3fff8ea79153f1b2a574b140c442690bc5ea5ef8
earizon <enrique.arizonbenito@gmail.com>
2016-10-20 11:09:51 +0200

    Fix test

--- java-ilp-core/src/test/java/org/interledger/ilp/core/TransferIDTest.java
+++ java-ilp-core/src/test/java/org/interledger/ilp/core/TransferIDTest.java
@@ -9,9 +9,14 @@ public class TransferIDTest {
     // TODO: Add more test (compare objects, ...)
     @Test
     public void testCreateTransferID() {
+        new TransferID("http://ledger:3000/transfers/3a2a1d9e-8640-4d2d-b06c-84f2cd613204");
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void testCreateTransferIDWithWrongURI() {
         new TransferID("3a2a1d9e-8640-4d2d-b06c-84f2cd613204");
     }
-    
+
     @Test(expected=RuntimeException.class)
     public void testCreateDTTMWrongFormat() {
         new TransferID("1234");
