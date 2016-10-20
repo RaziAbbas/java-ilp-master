44a84ef5b423746c378febf910e776008f277ff9
earizon <enrique.arizonbenito@gmail.com>
2016-10-20 17:40:50 +0200

    Fix regression / broken test

--- java-ilp-common-api/src/test/java/org/interledger/ilp/common/api/util/JsonObjectBuilderTest.java
+++ java-ilp-common-api/src/test/java/org/interledger/ilp/common/api/util/JsonObjectBuilderTest.java
@@ -16,6 +16,17 @@ public class JsonObjectBuilderTest {
 
     private static class MyBean {
 
+        String string;
+        
+        @SuppressWarnings("unused")
+        public String getString() {
+            return string;
+        }
+        
+        @SuppressWarnings("unused")
+        public void setString(String string) {
+            this.string = string;
+        }
 
     }
 
@@ -42,6 +53,7 @@ public class JsonObjectBuilderTest {
     public void testFrom() {
         System.out.println("testFrom");
         MyBean value = new MyBean();
+        value.setString("value");
         JsonObject expResult = new JsonObject("{ \"string\": \"value\"}");
         JsonObject result = instance.from(value).get();
         assertEquals(expResult, result);
@@ -70,6 +82,7 @@ public class JsonObjectBuilderTest {
     public void testFromWith() {
         System.out.println("testFromWith");
         MyBean value = new MyBean();
+        value.setString("value");
         String expJson = "{ \"string\": \"value\",\"field2\":123}";
         JsonObject expResult = new JsonObject(expJson);
         JsonObject result = instance.from(value).with("field2", 123L).get();
