d47746a3762e7290ab18a1aecfa1679124714027
earizon <enrique.arizonbenito@gmail.com>
2016-10-27 10:13:11 +0200

    Clean code. Remove old prototype code from Ledger interface and SimpleLeger implementation

--- java-ilp-ledger-simple/src/main/java/org/interledger/ilp/ledger/impl/simple/SimpleLedger.java
+++ java-ilp-ledger-simple/src/main/java/org/interledger/ilp/ledger/impl/simple/SimpleLedger.java
@@ -1,11 +1,7 @@
 package org.interledger.ilp.ledger.impl.simple;
 
-import org.interledger.cryptoconditions.Fulfillment;
 import org.interledger.ilp.core.Ledger;
 import org.interledger.ilp.core.LedgerInfo;
-import org.interledger.ilp.core.LedgerTransfer;
-import org.interledger.ilp.core.LedgerTransferRejectedReason;
-import org.interledger.ilp.core.events.LedgerEventHandler;
 import org.interledger.ilp.common.config.Config;
 
 /**
@@ -40,55 +36,4 @@ public class SimpleLedger implements Ledger {
         return config;
     }
 
-    @Override
-    public void send(LedgerTransfer transfer) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void rejectTransfer(LedgerTransfer transfer, LedgerTransferRejectedReason reason) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void fulfillCondition(Fulfillment fulfillment) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void registerEventHandler(LedgerEventHandler<?> handler) {
-        // TODO Auto-generated method stub
-        
-    }
-
-//    public void send(LedgerTransfer transfer) {
-//        LedgerAccountManager accountManager = LedgerAccountManagerFactory.getLedgerAccountManagerSingleton();
-//        LedgerAccount debitFrom = accountManager.getAccountByName(transfer. getDebits()[0].account.getAccountId());
-//        LedgerAccount creditTo  = accountManager.getAccountByName(transfer.getCredits()[0].account.getAccountId());
-//        if (creditTo.equals(debitFrom)) {
-//            throw new RuntimeException("accounts are the same");
-//        }
-//        MonetaryAmount amount = transfer.getCredits()[0].amount;
-//        if (debitFrom.getBalance().isGreaterThanOrEqualTo(amount)) {
-//            debitFrom.debit(amount);
-//            creditTo.credit(amount);
-//        } else {
-//            throw new InterledgerException(InterledgerException.RegisteredException.InsufficientAmountError, amount.toString());
-//        }
-//    }
-
-//    public void rejectTransfer(LedgerTransfer transfer, LedgerTransferRejectedReason reason) {
-//        throw new UnsupportedOperationException("Not supported yet.");
-//    }
-
-//    public void fulfillCondition(Fulfillment fulfillment) {
-//        throw new UnsupportedOperationException("Not supported yet.");
-//    }
-//
-//    public void registerEventHandler(LedgerEventHandler<?> handler) {
-//        throw new UnsupportedOperationException("Not supported yet.");
-//    }
 }
