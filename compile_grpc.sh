5f738c18524af9147335d8c82504a490f49a1bc4
earizon <enrique.arizonbenito@gmail.com>
2016-12-28 14:50:38 +0100

    Fix script

--- compile_grpc.sh
+++ compile_grpc.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 if [ ! -d ledger-grpc/ILP_gRPC_API ] ; then
+    pushd .
     cd ledger-grpc && git clone https://github.com/everis-innolab/ILP_gRPC_API
+    popd
 fi
+
+gradle :ledger-grpc:build
